6e137b323c72fe3f52b31ee94b356798368f9422
LikOfTheLichKing <92628755+LikOfTheLichKing@users.noreply.github.com>
2023-10-02 11:25:34 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include math.h
+int main(){
+  double x = std::cin>>x;
+  double a = 3.7;
+  double p=x*x - sqrt(fabs(x));
+  double t = pow(x+a*a, 1/3);
+  double y=p*p + t*t*t*t;
+  cout<<y;
+}
